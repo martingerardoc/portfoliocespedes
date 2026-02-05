488957fea077a316e9feae1f35bd2c0395bf6d35
martingerardoc <martingerardoc56@gmail.com>
2026-02-05 11:45:42 -0300

    Primer commmit

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+    plugins: [
+        tailwindcss(),
+    ],
+build:{
+    rollupOptions:{
+        input:{
+            main: resolve(__dirname,'index.html'),
+            about: resolve(__dirname,'pages/about.html'),
+            contact: resolve(__dirname,'pages/contact.html'),
+        }
+    }
+}
+})
